c8693d0fc15c1a9693f5e4fa25fe8e84ba10880b
Adrian Popa <popadrian94@gmail.com>
2019-05-13 18:48:36 +0300

    Last week's

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,55 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
-router.post('/add', function (req, res, next) {
+// http://localhost:3000/users/add
+router.post('/add', function(req, res, next) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
   console.warn('add', firstName, lastName, phone);
+  
+  var persons = require('../public/data/persons.json');
+  //var strPersons = fs.readFileSync('./public/data/persons.json');
+  //var persons = JSON.parse(strPersons);
+  
+  persons.push({
+    firstName,
+    lastName,
+    phone
+  });
+  
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
 
   res.json({
     success: true,
-    message: 'TODO'
+    message: 'Done!'
+  });
+});
+
+
+router.delete('/delete', function(req, res, next) {
+  var id = req.body.id;
+  console.warn('remove person', id);
+  
+  var persons = require('../public/data/persons.json');
+  
+  var remainingPersons = persons.filter(function(person) {
+    return person.id != id; 
+  });
+  
+  var str = JSON.stringify(remainingPersons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
+
+  res.json({
+    success: true,
+    message: 'Done!'
   });
 });
 
